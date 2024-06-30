4743f0edc07bf0d2276e57fd84f68374ba3d0943
Stephen L. <LRQ3000@gmail.com>
2024-06-30 11:30:50 +0200

    build: fix reorientation_registration_helper-cli.py commandline call
    Signed-off-by: Stephen L. <LRQ3000@gmail.com>

--- reorientation_registration_helper-cli.py
+++ reorientation_registration_helper-cli.py
@@ -1,4 +1,4 @@
 # CLI interface to launch pathmatcher directly, mostly useful to build a pyInstaller binary
-from pathmatcher import reorientation_registration_helper
+from pathmatcher.reorientation_registration_helper import main as reorientation_registration_helper
 if __name__ == '__main__':
     reorientation_registration_helper()
